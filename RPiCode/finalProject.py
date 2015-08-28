51590bb145f8b7a4e959f125caa42a7a69f28f3b
Brendon Chen <brc019@ucsd.edu>
2015-08-28 14:44:15 -0700

    Updated finalProject.py

--- RPiCode/finalProject.py
+++ RPiCode/finalProject.py
@@ -1,11 +1,14 @@
 # Final RPi Project for SPIS 2015
 
-import RPi.GPIO as GPIO, time
+import RPi.GPIO as GPIO, time, sys
 
 # use physical pin numbering
 GPIO.setmode(GPIO.BOARD)
 
-#set up digital line detectors as inputs
+# set up digital line detectors as inputs
+# Pin 11 Right Sensor
+# Pin 12 Middle Sensor
+# Pin 13 Left Sensor
 GPIO.setup(11, GPIO.IN)
 GPIO.setup(12, GPIO.IN)
 GPIO.setup(13, GPIO.IN)
@@ -27,12 +30,19 @@ GPIO.setup(26, GPIO.OUT)
 b = GPIO.PWM(26,20)
 b.start(0)
 
+# Define duty cycles as speed options
 slowspeed = 20
 fastspeed = 100
+
+
+# Define pins for LEDs
 LED2 = 22
 LED3 = 18
 GPIO.setup(LED2, GPIO.OUT)
 GPIO.setup(LED3, GPIO.OUT)
+
+# Define Sonar pin for Trigger and Echo to be the same
+SONAR = 8
 
 def rightForward(speed):
   p.ChangeDutyCycle(speed)
@@ -67,10 +77,12 @@ def backward(speed):
 def turnRight():
   rightForward(slowspeed)
   leftForward(fastspeed)
+  print('turn right')
 
 def turnLeft():
   rightForward(fastspeed)
   leftForward(slowspeed)
+  print('turn left')
 
 def stopall():
   p.ChangeDutyCycle(0)
@@ -79,11 +91,50 @@ def stopall():
   b.ChangeDutyCycle(0)
   print('stop')
 
-turnRight()
-time.sleep(5)
-stopall()
-turnLeft()
-time.sleep(5)
-stopall()
+def followLine():
+  if GPIO.input(11) == 0 and GPIO.input(12) == 1 and GPIO.input(13) == 0:
+    forward(fastspeed)
+    setLEDs(0, 0)
+  elif GPIO.input(11) == 1:
+    turnRight()
+    setLEDs(1, 0)
+  elif GPIO.input(13) == 1:
+    turnLeft()
+    setLEDs(0, 1)
 
-GPIO.cleanup()
+def sonar():
+  GPIO.setup(SONAR, GPIO.OUT)
+  GPIO.output(SONAR, True)
+  time.sleep(0.00001)
+  GPIO.output(SONAR, False)
+  start = time.time()
+  count = time.time()
+  GPIO.setup(SONAR, GPIO.IN)
+  while GPIO.input(SONAR) == 0 and time.time() - count < 0.1:
+    start = time.time()
+  stop = time.time()
+  while GPIO.input(SONAR) == 1:
+    stop = time.time()
+  # Calculate pulse length
+  elapsed = stop - start
+  # Distance pulse travelled in that time is time
+  # multiplied by the speed of sound (cm/s)
+  distance = elapsed * 34000
+  # That was the distance there and back so halve the value
+  distance = distance / 2
+  print 'Distance:', distance
+  time.sleep(1)
+
+def setLEDs(L2, L3):
+  GPIO.output(LED2, L2)
+  GPIO.output(LED3, L3)
+
+setLEDs(1, 1)
+
+try:
+  while True:
+    followLine()
+
+except KeyboardInterrupt:
+  GPIO.cleanup()
+  sys.exit()
